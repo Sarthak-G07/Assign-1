fd785773010699ae6f33578911bc86aebd22cc74
Sarthak-G07 <22103271@mail.jiit.ac.in>
2024-07-27 09:52:26 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,1 +1,14 @@
-#include
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    cout<<"enter size = "
+    cin>>n;
+    cout<<"enter elements"<<endl
+    for(int i=0; i<n; i++){
+            cin
+    }
+
+}
